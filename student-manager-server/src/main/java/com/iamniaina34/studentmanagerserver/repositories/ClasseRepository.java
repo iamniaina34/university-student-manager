077b619ac5e72296354d453fd29b3e3fc9e17828
iamniaina34 <iammandrindraniaina34@gmail.com>
2024-05-20 17:31:23 +0300

    Meh meh meh

--- student-manager-server/src/main/java/com/iamniaina34/studentmanagerserver/repositories/ClasseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.iamniaina34.studentmanagerserver.repositories;
-
-import com.iamniaina34.studentmanagerserver.models.Classe;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClasseRepository extends JpaRepository<Classe, Integer> {
-}
